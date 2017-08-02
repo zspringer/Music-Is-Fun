088a858f1445b6e18566bd55c73f5812f981d0b2
Mark Ohnsman <Mark.Ohnsman@gmail.com>
2017-08-02 08:55:38 -0600

    adjusments for summer 17

---
+++ itunes-controller.js
@@ -0,0 +1,17 @@
+function ItunesController(){
+  var itunesService = new ItunesService()
+  //Do Not Modify the getMusic function
+  this.getMusic = function getMusic(e){
+    e.preventDefault();
+    var artist = e.target.artist.value;
+    itunesService.getMusicByArtist(artist).then(); //after get music by artist returns what are you doing with the objects?
+  }
+
+  //Start coding here
+
+
+
+
+
+  
+}
--- itunes-service.js
+++ itunes-service.js
@@ -1,13 +1,16 @@
 function ItunesService(){
 
-    this.getMusicByArtist = function(artist, cb) {
-      
+    this.getMusicByArtist = function(artist) {
+
+      //allows requests to localhost: 8080 otherwise blocked by itunes
       var url = '//bcw-getter.herokuapp.com/?url=';
       var url2 = 'https://itunes.apple.com/search?term=' + artist;
       var apiUrl = url + encodeURIComponent(url2);
       
+      //changes the button to loading while songs load
       $('#get-music-button').text('LOADING....');
       
+      //modifies the objects to reduce the excess data
       return $.getJSON(apiUrl).then(function(response){
         var songList = response.results.map(function (song) {
                   return {
@@ -19,6 +22,7 @@ function ItunesService(){
                       preview: song.previewUrl
                     };
                 })
+        //changes button back to GET MUSIC once songs are loaded
         $('#get-music-button').text('GET MUSIC');
         return songList;
       })
